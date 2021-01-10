ec8f91a14ccbbb04ad23c3fc428cf05e2c017b43
eau300 <eau300@gmail.com>
2021-01-10 15:13:34 +0800

    P3

--- QQ.java
+++ QQ.java
@@ -3,6 +3,8 @@ package xxx;
 public class HelloEclipse {
 	private int QQ;
   	private String yoyo;
+	private int QW;
+	
 	
 	public static void main(String[]args) {
 		System.out.println("Hello Eclipse");
